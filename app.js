35f91410f9c7403b0b94bca7b2c42895e07fe86f
lBLACKl <blacktroop2@gmail.com>
2023-05-05 16:33:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#fff';
+tg.MainButton.color = '#0088cc';
+
+let item = '';
+
+let buttons = document.querySelectorAll('.item__button');
+buttons.forEach((button, index) => {
+    button.addEventListener('click', event => {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.setRangeText('Вы выбрали товар ' + (index+1));
+            item = '1';
+            tg.MainButton.show();
+        }
+    })
+});
+
+Telegram.WebApp.onEvent('mainButtonClick', () => {
+    tg.sendData(item);
+});
+
+
+let usercard = document.querySelector('.user_card');
+
+let p = document.createElement('p');
+
+p.innerHTML = `${tg.initDataUnsafe.user.first_name}` +
+            + `<br>` +
+            + `${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
